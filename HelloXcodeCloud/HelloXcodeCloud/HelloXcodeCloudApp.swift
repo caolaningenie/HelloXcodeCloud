ffc3cfdbf0d7fcbd1fea17c6faacde695cbc6ee8
caolaningenie <79855506+caolaningenie@users.noreply.github.com>
2022-11-16 10:20:16 +0000

    Created project.

---
+++ HelloXcodeCloud/HelloXcodeCloud/HelloXcodeCloudApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloXcodeCloudApp.swift
+//  HelloXcodeCloud
+//
+//  Created by Caolan Owens on 16/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct HelloXcodeCloudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
